d0d0917f15fdc585cca94dd3de5458057519e876
igor-laptop <bistrovic.igor@gmail.com>
2020-09-22 19:05:10 +0200

    1

---
+++ Kockica/Assets/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000;
+    public float sideForce = 500f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if( Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow) )
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if( Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow) )
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
