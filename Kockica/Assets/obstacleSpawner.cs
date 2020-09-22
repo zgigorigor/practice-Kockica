53f43e75518dc82bab4b8afd5ae98bb870847745
igor-laptop <bistrovic.igor@gmail.com>
2020-09-22 20:31:00 +0200

    obstacle spawner

---
+++ Kockica/Assets/obstacleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class obstacleSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject obstaclePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int noObsticale = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if ( noObsticale != i )
+            {
+                Instantiate(obstaclePrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
